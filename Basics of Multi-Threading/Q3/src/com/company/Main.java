d98b0fe59191994fbc99906f16d0752efa60dceb
Aman-Pareek-TTN <78476294+Aman-Pareek-TTN@users.noreply.github.com>
2021-02-24 13:19:23 +0530

    "Correct Committing Q3 again"

--- Basics of Multi-Threading/Q3/src/com/company/Main.java
+++ Basics of Multi-Threading/Q3/src/com/company/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        ExecutorService executorService= Executors.newFixedThreadPool(2);
+        ExecutorService executorService= Executors.newSingleThreadExecutor();
 
         for(int i=0;i<5;i++)
             executorService.submit(new MyThread(i));
